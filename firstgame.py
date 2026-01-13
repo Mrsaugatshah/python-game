c17583499ec3c4c87e5903b2791063f2c7051463
Mrsaugatshah <saugats082@gmail.com>
2026-01-13 20:23:03 +0545

    animation added

--- firstgame.py
+++ firstgame.py
@@ -5,10 +5,14 @@ screen=pygame.display.set_mode((800,400))
 pygame.display.set_caption("runner")
 clock=pygame.time.Clock()
 test_surface=pygame.Surface((100,200))
-sky_surface=pygame.image.load('sky.webp')
-ground_surface=pygame.image.load('ground.webp')
+sky_surface=pygame.image.load('sky.webp').convert_alpha()
+ground_surface=pygame.image.load('ground.webp').convert_alpha()
 test_font=pygame.font.Font(None,50)
 text_surface=test_font.render('my game',False,'Green')
+snail_surface=pygame.image.load('snail.jpg').convert_alpha()
+snail_rect=snail_surface.get_rect(bottomright =(600,300))
+player_surface=pygame.image.load('charecter.webp').convert_alpha()
+player_rectangle=player_surface.get_rect(topleft=(80,300))
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -17,6 +21,10 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,350))
     screen.blit(text_surface,(300,50))
+    screen.blit(player_surface,(80,300))
+    snail_rect.x-=4
+    if snail_rect.right <=0: snail_rect.left=800
+    screen.blit(snail_surface,snail_rect)
     
     #draw all
     #update everything
