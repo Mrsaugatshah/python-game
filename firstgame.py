9207fb2895b13ce191932b34b3412360d9d56652
Mrsaugatshah <saugats082@gmail.com>
2026-01-15 20:55:09 +0545

    jump

--- firstgame.py
+++ firstgame.py
@@ -9,6 +9,7 @@ sky_surface=pygame.image.load('sky.webp').convert_alpha()
 ground_surface=pygame.image.load('ground.webp').convert_alpha()
 test_font=pygame.font.Font(None,50)
 text_surface=test_font.render('my game',False,'Green')
+
 snail_surface=pygame.image.load('snail.jpg').convert_alpha()
 snail_rect=snail_surface.get_rect(bottomright =(600,300))
 player_surface=pygame.image.load('charecter.webp').convert_alpha()
@@ -18,8 +19,13 @@ while True:
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type==pygame.MOUSEMOTION:
-             if player_rectangle.collidepoint(event.pos):print('collesion')   
+        # if event.type==pygame.MOUSEMOTION:
+        #      if player_rectangle.collidepoint(event.pos):print('collesion')  
+        if event.type==pygame.KEYDOWN:
+             if event.key==pygame.K_SPACE:
+                 print('jump')
+             if event.type==pygame.KEYUP:
+                print('keyup')
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,350))
     screen.blit(text_surface,(300,50))
@@ -27,6 +33,7 @@ while True:
     snail_rect.x-=4
     if snail_rect.right <=0: snail_rect.left=800
     screen.blit(snail_surface,snail_rect)
+    
     
     # if player_rectangle.colliderect(snail_rect):
     #     print('collision')
